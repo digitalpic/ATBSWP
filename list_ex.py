6e51e3c81ee49907cc38cbc3a7f225c0c3ed6a96
Shawn Conrad <Shawn Conrad>
2016-11-22 15:19:08 -0600

    methods

---
+++ list_ex.py
@@ -0,0 +1,3 @@
+supplies = ['pens', 'staplers', 'bazookas', 'machetes']
+for i in range(len(supplies)):
+    print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
---
+++ methods_ex.py
@@ -0,0 +1,3 @@
+spam = ['hello', 'hi', 'howdy', 'hey']
+# spam.index('hey')
+print('This value is ' + str(spam.index('hey')))
