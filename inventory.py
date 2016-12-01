de3a9a9ed2979885ae40d0052c6c790bdfdcce9e
Shawn Conrad <Shawn Conrad>
2016-12-01 13:26:58 -0600

    .

--- inventory.py
+++ inventory.py
@@ -10,3 +10,17 @@ def displayInventory(inventory):
     print('Total number of items: ' + str(itemTotal))
 
 displayInventory(stuff)
+
+
+def addToInventory(inventory, addedItems):
+    for i in range(len(addedItems)):
+        inventory.setdefault(addedItems[i], 0)
+        inventory[addedItems[i]] = inventory[addedItems[i]] + 1
+    return inventory
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+
+displayInventory(inv)
